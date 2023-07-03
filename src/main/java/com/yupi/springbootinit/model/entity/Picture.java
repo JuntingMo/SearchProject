248dbf214ee0524ec6f7df49111a6cf4374ca53b
MOJunting <1226008941@qq.com>
2023-07-04 02:15:47 +0800

    The second day of Project. 做了前后端调联的后端，跑通了爬取网页小黑子信息

---
+++ src/main/java/com/yupi/springbootinit/model/entity/Picture.java
@@ -0,0 +1,14 @@
+package com.yupi.springbootinit.model.entity;
+
+import lombok.Data;
+
+/**
+ * 图片
+ */
+
+@Data
+public class Picture {
+    private String title;
+    private String url;
+    private static final long serialVersionUID = 1L;
+}
