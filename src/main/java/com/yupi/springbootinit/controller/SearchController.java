809b5f2d146a97e3b940eddab4af539d175fdf45
MOJunting <1226008941@qq.com>
2023-07-05 16:01:41 +0800

    新branch，完成到视频49分32秒，开发到适配器模式之前

--- src/main/java/com/yupi/springbootinit/controller/SearchController.java
+++ src/main/java/com/yupi/springbootinit/controller/SearchController.java
@@ -6,11 +6,13 @@ import com.yupi.springbootinit.common.ErrorCode;
 import com.yupi.springbootinit.common.ResultUtils;
 import com.yupi.springbootinit.exception.BusinessException;
 import com.yupi.springbootinit.exception.ThrowUtils;
+import com.yupi.springbootinit.manager.SearchFacade;
 import com.yupi.springbootinit.model.dto.picture.PictureQueryRequest;
 import com.yupi.springbootinit.model.dto.post.PostQueryRequest;
 import com.yupi.springbootinit.model.dto.search.SearchRequest;
 import com.yupi.springbootinit.model.dto.user.UserQueryRequest;
 import com.yupi.springbootinit.model.entity.Picture;
+import com.yupi.springbootinit.model.enums.SearchTypeEnum;
 import com.yupi.springbootinit.model.vo.PostVO;
 import com.yupi.springbootinit.model.vo.SearchVO;
 import com.yupi.springbootinit.model.vo.UserVO;
@@ -18,6 +20,7 @@ import com.yupi.springbootinit.service.PictureService;
 import com.yupi.springbootinit.service.PostService;
 import com.yupi.springbootinit.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,65 +48,11 @@ public class SearchController {
     @Resource
     private PictureService pictureService;
 
+    @Resource
+    private SearchFacade searchFacade;
+
     @PostMapping("/all")
     public BaseResponse<SearchVO> searchAll(@RequestBody SearchRequest searchRequest, HttpServletRequest request) {
-//        //非多线程
-//        String searchText = searchRequest.getSearchText();
-//        Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
-//
-//        UserQueryRequest userQueryRequest = new UserQueryRequest();
-//        userQueryRequest.setUserName(searchText);
-//        Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
-//
-//        PostQueryRequest postQueryRequest = new PostQueryRequest();
-//        postQueryRequest.setSearchText(searchText);
-//        Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
-//
-//        SearchVO searchVO = new SearchVO();
-//        searchVO.setUserList(userVOPage.getRecords());
-//        searchVO.setPostList(postVOPage.getRecords());
-//        searchVO.setPictureList(picturePage.getRecords());
-//
-//        return ResultUtils.success(searchVO);
-
-        //多线程
-        String searchText = searchRequest.getSearchText();
-
-        CompletableFuture<Page<UserVO>> userTask = CompletableFuture.supplyAsync(() -> {
-            UserQueryRequest userQueryRequest = new UserQueryRequest();
-            userQueryRequest.setUserName(searchText);
-            Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
-            return userVOPage;
-        });
-
-        CompletableFuture<Page<PostVO>> postTask = CompletableFuture.supplyAsync(() -> {
-            PostQueryRequest postQueryRequest = new PostQueryRequest();
-            postQueryRequest.setSearchText(searchText);
-            Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
-            return postVOPage;
-        });
-
-        CompletableFuture<Page<Picture>> pictureTask = CompletableFuture.supplyAsync(() -> {
-            Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
-            return picturePage;
-        });
-
-        CompletableFuture.allOf(userTask, postTask, pictureTask).join();
-        try {
-            Page<UserVO> userVOPage = userTask.get();
-            Page<PostVO> postVOPage = postTask.get();
-            Page<Picture> picturePage = pictureTask.get();
-
-            SearchVO searchVO = new SearchVO();
-            searchVO.setUserList(userVOPage.getRecords());
-            searchVO.setPostList(postVOPage.getRecords());
-            searchVO.setPictureList(picturePage.getRecords());
-            return ResultUtils.success(searchVO);
-        } catch (Exception e) {
-            log.error("查询异常", e);
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "查询异常");
-        }
-
+        return ResultUtils.success(searchFacade.searchAll(searchRequest,request));
     }
-
 }
--- src/main/java/com/yupi/springbootinit/model/dto/search/SearchRequest.java
+++ src/main/java/com/yupi/springbootinit/model/dto/search/SearchRequest.java
@@ -19,5 +19,10 @@ public class SearchRequest extends PageRequest implements Serializable {
      */
     private String searchText;
 
+    /**
+     *类型
+     */
+    private String type;
+
     private static final long serialVersionUID = 1L;
 }
